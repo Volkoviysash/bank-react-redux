7fb2779b1ca227b5e1507da1255adf74109839cb
volkovich <volkoviysash@gmail.com>
2023-04-26 05:07:27 +0300

    feat: Add Redux and clear garbage.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  // Create dispatch
+  const dispatch = useDispatch();
+
+  //to take state from Redux
+  const cash = useSelector((state) => state.cash);
+
+  const getCash = () => {
+    dispatch({ type: "GET_CASH", payload: 5 });
+  };
+
+  const addCash = () => {
+    dispatch({ type: "ADD_CASH", payload: 5 });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">${cash}</div>
+      <div style={{ display: "flex" }}>
+        <button onClick={() => addCash()} className="cash-in">
+          Cash IN
+        </button>
+        <button onClick={() => getCash()} className="cash-out">
+          Cash OUT
+        </button>
+      </div>
     </div>
   );
 }
